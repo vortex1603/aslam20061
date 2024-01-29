91359d3f919f1401759fd5e8d5ffeea9210a795d
vortex1603 <amahmudov257@gmail.com>
2024-01-29 15:49:19 +0500

    с баланс

--- goodbye.js
+++ goodbye.js
@@ -5,21 +5,21 @@ if (name === 'Alex') {
     let pin = prompt('пин-код');
     
     if (pin >= 7777) {
-        // let balance = prompt('сколько ты хочешь снимать');
+        let balance = prompt('сколько ты хочешь снимать');
         
-        // if (balance <= 10000 ) {
-        //     console.log('Спасибо');
-        //     alert('Спасибо');
+        if (balance <= 10000 ) {
+            console.log('Спасибо');
+            alert('Спасибо');
 
-        //     let initialBalance = 10000;
-        //     let remainingBalance = initialBalance - balance;
+            let initialBalance = 10000;
+            let remainingBalance = initialBalance - balance;
 
-        //     console.log(`Остаток на карте: ${remainingBalance}`);
-        //     alert(`Остаток на карте: ${remainingBalance}, снято ${balance}`);
-        // } else {
-        //     console.log('недостаточно средств');
-        //     alert('недостаточно средствd');
-        // }
+            console.log(`Остаток на карте: ${remainingBalance}`);
+            alert(`Остаток на карте: ${remainingBalance}, снято ${balance}`);
+        } else {
+            console.log('недостаточно средств');
+            alert('недостаточно средствd');
+        }
     } else {
         console.log('Попробуйте еще раз');
         alert('Попробуйте еще раз');
